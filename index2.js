5354a30c7115821744094380ec7d82283350a119
Jayden-Ferrao <jayden.ferrao@kilowott.com>
2024-06-21 09:21:36 +0530

    uploaded

---
+++ index2.js
@@ -0,0 +1,43 @@
+// Function to check if an element is in viewport
+function isInViewport(element) {
+    const rect = element.getBoundingClientRect();
+    return (
+        rect.top >= 0 &&
+        rect.left >= 0 &&
+        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+        rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+    );
+}
+// Function to handle scroll event
+function handleScroll() {
+    const contactSection = document.getElementById('contact');
+    if (isInViewport(contactSection)) {
+        contactSection.classList.add('slide-up');
+        // Remove the event listener once animation is applied
+        window.removeEventListener('scroll', handleScroll);
+    }
+}
+// Add scroll event listener to trigger animation
+window.addEventListener('scroll', handleScroll);
+
+
+
+
+ // Check if sign in is active
+$(document).ready(function() {
+    var hash = window.location.hash;
+    if (hash && hash === '#signin') {
+        $('#signin-link').addClass('active');
+    }
+});
+
+
+
+
+// JavaScript to handle active class for navbar items
+$(document).ready(function() {
+    $('.navbar-nav .nav-link').on('click', function() {
+    $('.navbar-nav .nav-link').removeClass('active');
+    $(this).addClass('active');
+    });
+ });
