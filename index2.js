ed0d9ac837b3a8afabc9ac84c86d39f354423d14
Jayden-Ferrao <jayden.ferrao@kilowott.com>
2024-06-26 09:55:44 +0530

    Updated JS

--- index2.js
+++ index2.js
@@ -1,10 +1,18 @@
-// Function to handle scroll event
 function handleScroll() {
+    const aboutSection = document.getElementById('about');
     const contactSection = document.getElementById('contact');
+
+    if (isInViewport(aboutSection)) {
+        aboutSection.classList.add('slide-up');
+    }
+
     if (isInViewport(contactSection)) {
         contactSection.classList.add('slide-up');
+    }
+
+    if (aboutSection.classList.contains('slide-up') && contactSection.classList.contains('slide-up')) {
         window.removeEventListener('scroll', handleScroll);
     }
 }
-window.addEventListener('scroll', handleScroll);
 
+window.addEventListener('scroll', handleScroll);
