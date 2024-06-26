d28c7274f6d674a331136c76225f0612a8335897
Jayden-Ferrao <jayden.ferrao@kilowott.com>
2024-06-26 09:39:03 +0530

    Updated JS

--- index2.js
+++ index2.js
@@ -8,6 +8,7 @@ function isInViewport(element) {
         rect.right <= (window.innerWidth || document.documentElement.clientWidth)
     );
 }
+
 // Function to handle scroll event
 function handleScroll() {
     const contactSection = document.getElementById('contact');
@@ -17,19 +18,3 @@ function handleScroll() {
     }
 }
 window.addEventListener('scroll', handleScroll);
-
- // Check if sign in is active
-$(document).ready(function() {
-    var hash = window.location.hash;
-    if (hash && hash === '#signin') {
-        $('#signin-link').addClass('active');
-    }
-});
-
-// JavaScript to handle active class for navbar items
-$(document).ready(function() {
-    $('.navbar-nav .nav-link').on('click', function() {
-    $('.navbar-nav .nav-link').removeClass('active');
-    $(this).addClass('active');
-    });
- });
