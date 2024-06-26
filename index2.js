177122e72e51c2fbbaec02419088c7cdedee8fcd
Jayden-Ferrao <jayden.ferrao@kilowott.com>
2024-06-26 09:43:48 +0530

    Updated JS

--- index2.js
+++ index2.js
@@ -1,14 +1,3 @@
-// Function to check if an element is in viewport
-function isInViewport(element) {
-    const rect = element.getBoundingClientRect();
-    return (
-        rect.top >= 0 &&
-        rect.left >= 0 &&
-        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
-        rect.right <= (window.innerWidth || document.documentElement.clientWidth)
-    );
-}
-
 // Function to handle scroll event
 function handleScroll() {
     const contactSection = document.getElementById('contact');
@@ -18,3 +7,4 @@ function handleScroll() {
     }
 }
 window.addEventListener('scroll', handleScroll);
+
