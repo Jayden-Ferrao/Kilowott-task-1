eec96f0d8d0d05d36c268fe38128de14348a2889
Jayden-Ferrao <jayden.ferrao@kilowott.com>
2024-07-03 10:14:23 +0530

    new Jquery fn added

--- index2.js
+++ index2.js
@@ -14,5 +14,30 @@ $('#contactForm').on('submit', function(event) {
     $('#contactForm')[0].reset();
 });
 
+$(document).ready(function() {
+    // Function to highlight the card
+    function highlightCard(card, callback) {
+        $(card).css('background-color', '#d4edda');
+        callback();
+    }
+
+    // Flag to track if the plan has been selected
+    var planSelected = false;
+
+    // Click event for the "Choose Plan" buttons
+    $('.btn-primary').one('click', function() { // .one() ensures the event happens only once
+        if (!planSelected) {
+            var card = $(this).closest('.card');
+            var planName = $(card).find('.card-title').text();
+
+            // Call the highlightCard function with a callback to show an alert
+            highlightCard(card, function() {
+                alert('You have selected the ' + planName);
+                planSelected = true; // Set the flag to true after selection
+            });
+        }
+    });
+});
+
 // JQuery: fadein, fadeout, slide, stop, callback, chaining, get-set-remove-add, ancestors-traversing-siblings
-// JS: hoisting, strict, methods, get-set, call-apply-bind, async-await, DOM-BOM
+// JS: hoisting, strict, methods, get-set, call-apply-bind, async-await, DOM-BOM, slideup-slidedown
