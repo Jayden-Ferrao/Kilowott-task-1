1dd88b4f33253407cbf5096592594d0820b7cb5e
Jayden-Ferrao <jayden.ferrao@kilowott.com>
2024-07-04 10:38:39 +0530

    load more feature

--- index2.js
+++ index2.js
@@ -13,5 +13,22 @@ $('#contactForm').on('submit', function(event) {
     alert(`Message sent successfully by ${name}!`);
     $('#contactForm')[0].reset();
 });
+
+// Load More functionality
+$(document).ready(function() {
+    var initialItems = 3;
+    var loadMoreItems = 3;
+    
+    // Hide items that exceed the initial count
+    $('#services-list .service-item:gt(' + (initialItems - 1) + ')').hide();
+
+    $('#loadMoreBtn').click(function() {
+        $('#services-list .service-item:hidden').slice(0, loadMoreItems).slideDown();
+        // If no more hidden items, hide the load more button
+        if ($('#services-list .service-item:hidden').length === 0) {
+            $('#loadMoreBtn').fadeOut('slow');
+        }
+    });
+});
 // JQuery: fadein, fadeout, slide, stop, callback, chaining, get-set-remove-add, ancestors-traversing-siblings
 // JS: hoisting, strict, methods, get-set, call-apply-bind, async-await, DOM-BOM, slideup-slidedown
