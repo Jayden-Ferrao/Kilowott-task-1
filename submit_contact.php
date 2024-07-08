d1754bfa0632a89d7192cabf84553e28692cb997
Jayden-Ferrao <jayden.ferrao@kilowott.com>
2024-07-08 13:20:59 +0530

    updated the php

--- submit_contact.php
+++ submit_contact.php
@@ -12,17 +12,19 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 $name = $_POST['name'];
 $email = $_POST['email'];
 $message = $_POST['message'];
-
 // Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
+
+
 // Prepare and bind SQL statement
 $stmt = $conn->prepare("INSERT INTO contacts (name, email, message) VALUES (?, ?, ?)");
 $stmt->bind_param("sss", $name, $email, $message);
 
 $stmt->execute();
-echo "Submission Succesfull!";
+header("Location: thankyou.html");
+exit;
 
 $stmt->close(); 
 $conn->close();
