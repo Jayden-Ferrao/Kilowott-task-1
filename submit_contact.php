607244f8ac035327ba302f518b9c8f8845e6c024
Jayden-Ferrao <jayden.ferrao@kilowott.com>
2024-07-08 13:26:56 +0530

    Submit form working

--- submit_contact.php
+++ submit_contact.php
@@ -5,26 +5,31 @@ $username = "root";
 $password = "";
 $dbname = "contact_form_db";
 
-// Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
-
 // Get form data
 $name = $_POST['name'];
 $email = $_POST['email'];
 $message = $_POST['message'];
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
 // Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-
 // Prepare and bind SQL statement
 $stmt = $conn->prepare("INSERT INTO contacts (name, email, message) VALUES (?, ?, ?)");
 $stmt->bind_param("sss", $name, $email, $message);
 
-$stmt->execute();
-header("Location: thankyou.html");
-exit;
+if ($stmt->execute()) {
+    // Redirect to thank you page
+    header("Location: thankyou.html");
+    exit();
+} else {
+    
+    echo "Error: " . $sql . "<br>" . $stmt->error;
+}
 
 $stmt->close(); 
 $conn->close();
