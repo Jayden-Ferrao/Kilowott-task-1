3b67462fe9d79b2d6705d36b41b999eab97c8a7c
Jayden-Ferrao <jayden.ferrao@kilowott.com>
2024-07-16 10:19:01 +0530

    test

--- P1.php
+++ P1.php
@@ -3,50 +3,49 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>If Else date</title>
+    <title>If Else Date</title>
 </head>
 <body>
-    <!-- HTML Form -->
-    <form method="post">
-        <label for="date">Enter date ion this format (dd-mm-yyyy):</label>
+    <form method="post" action="">
+        <label for="date">Enter a date in dd-mm-yy: </label>
         <input type="text" id="date" name="date" required>
-        <input type="submit" value="Submit">
+        <button type="submit">Submit</button>
     </form>
     <?php
-    // Check if form is submitted
-    if ($_REQUEST["REQUEST_METHOD"] == "POST") {
-        $date = $_POST['date'];
-        function extractMonthIfElse($date) {
-            $month = substr($date, 3, 2); // basically 3 is the index and 2 is no.of characters taken
-            if ($month == '01') {
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        $date = $_POST["date"];
+        function printMonthIfElse($date) {
+            $month = substr($date, 3, 2);
+            $monthInt = (int)$month;
+            if ($monthInt == 1) {
                 echo "It's January!";
-            } elseif ($month == '02') {
+            } elseif ($monthInt == 2) {
                 echo "It's February!";
-            } elseif ($month == '03') {
+            } elseif ($monthInt == 3) {
                 echo "It's March!";
-            } elseif ($month == '04') {
+            } elseif ($monthInt == 4) {
                 echo "It's April!";
-            } elseif ($month == '05') {
+            } elseif ($monthInt == 5) {
                 echo "It's May!";
-            } elseif ($month == '06') {
+            } elseif ($monthInt == 6) {
                 echo "It's June!";
-            } elseif ($month == '07') {
+            } elseif ($monthInt == 7) {
                 echo "It's July!";
-            } elseif ($month == '08') {
+            } elseif ($monthInt == 8) {
                 echo "It's August!";
-            } elseif ($month == '09') {
+            } elseif ($monthInt == 9) {
                 echo "It's September!";
-            } elseif ($month == '10') {
+            } elseif ($monthInt == 10) {
                 echo "It's October!";
-            } elseif ($month == '11') {
+            } elseif ($monthInt == 11) {
                 echo "It's November!";
-            } elseif ($month == '12') {
+            } elseif ($monthInt == 12) {
                 echo "It's December!";
             } else {
                 echo "Invalid";
             }
         }
-        extractMonthIfElse($date);
+        printMonthIfElse($date);
     }
     ?>
 </body>
